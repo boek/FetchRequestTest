c5b19290e5a1d137a215114070bba949cc10c6de
Jeff Boek <jeff@jeffboek.com>
2020-08-10 16:50:03 -0700

    Initial Commit

---
+++ Shared/FetchRequestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRequestTestApp.swift
+//  Shared
+//
+//  Created by Jeff Boek on 8/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FetchRequestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
