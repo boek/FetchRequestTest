37402bafb988e6deb74d5d149a71a588ac665a5e
Jeff Boek <jeff@jeffboek.com>
2020-08-10 17:36:05 -0700

    new

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,58 +8,43 @@
 import SwiftUI
 import CoreData
 
-struct CreatorView: View {
+struct ContentView: View {
     @Environment(\.managedObjectContext) var context
     @State var name: String = ""
-    @Binding var isVisible: Bool
-    
-    var body: some View {
-        HStack {
-            TextField("Title", text: $name)
-            Button("Save", action: {
-                let category = Category(context: context)
-                category.id = UUID()
-                category.name = name
-                self.isVisible = false
-            })
-        }.navigationTitle("Create a category!").padding()
-    }
-}
 
-struct ContentView: View {
     @FetchRequest(
         entity: Category.entity(),
         sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)]
     ) var categories: FetchedResults<Category>
-    @State var showCreator = false
 
     var body: some View {
         NavigationView {
-            List {
-                if (categories.isEmpty) {
-                    Text("No Categories")
-                } else {
-                    ForEach(categories) { category in
-                        NavigationLink(category.name!, destination: Text(category.name!))
-                    }
-                }
-                NavigationLink(
-                    destination: CreatorView(isVisible: $showCreator),
-                    isActive: $showCreator,
-                    label: { EmptyView() }
-                )
-            }.listStyle(SidebarListStyle())
-            .toolbar(items: {
-                ToolbarItem {
-                    Button(action: { self.showCreator = true }, label: {
+            VStack {
+                HStack {
+                    TextField("New Category", text: $name)
+                    Button(action: createCategory, label: {
                         Image(systemName: "plus")
                     })
-                }
-            })
-            
-            Text("Hello!")
-            
+                }.padding()
+                List {
+
+                    if (categories.isEmpty) {
+                        Text("No Categories")
+                    } else {
+                        ForEach(categories) { category in
+                            NavigationLink(category.name!, destination: Text(category.name!))
+                        }
+                    }
+                }.listStyle(SidebarListStyle())
+            }
         }
+    }
+    
+    private func createCategory() {
+        let category = Category(context: context)
+        category.id = UUID()
+        category.name = name
+        name = ""
     }
 }
 
